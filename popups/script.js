98a93b4ffc4ef156ee6dd3fc02d3438f6f6290ec
ElliNet13 (PC) <elliotmoose4@gmail.com>
2023-08-27 18:45:18 -0400

    Pop-up

--- popups/script.js
+++ popups/script.js
@@ -1,17 +1,22 @@
-// Get references to the HTML elements
 const colorChooser = document.getElementById("colorChooser");
-const hexInput = document.getElementById("hexInput");
 const createPopupButton = document.getElementById("createPopupButton");
-
 // Add a click event listener to the button
 createPopupButton.addEventListener("click", function () {
+// Get references to the HTML elements
     // Get the selected color from the color chooser
     const selectedColor = colorChooser.value;
 
-    // Or, if a hex color is provided, use that instead
-    const hexColor = hexInput.value || selectedColor;
+    const hexColor = selectedColor;
 
     // Create a new pop-up window with the selected color as the background
     const popup = window.open("", "_blank", "width=200,height=200");
     popup.document.body.style.backgroundColor = hexColor;
 });
+function createPopup() {
+
+    // Define the properties of the popup window
+    var popupProperties = "width=272.80,height=186.20,resizable=no";
+
+    // Open the popup window
+    var popup = window.open("popup.html", "Popup", popupProperties);
+}
