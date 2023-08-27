32cb6b45afa4c1c29e2907a3d3e6a96839510d2b
ElliNet13 (PC) <elliotmoose4@gmail.com>
2023-08-27 18:48:42 -0400

    Fix

--- popups/script.js
+++ popups/script.js
@@ -12,11 +12,3 @@ createPopupButton.addEventListener("click", function () {
     const popup = window.open("", "_blank", "width=200,height=200");
     popup.document.body.style.backgroundColor = hexColor;
 });
-function createPopup() {
-
-    // Define the properties of the popup window
-    var popupProperties = "width=272.80,height=186.20,resizable=no";
-
-    // Open the popup window
-    var popup = window.open("popup.html", "Popup", popupProperties);
-}
---
+++ popups/script1.js
@@ -0,0 +1,8 @@
+function createPopup() {
+
+    // Define the properties of the popup window
+    var popupProperties = "width=272.80,height=186.20,resizable=no";
+
+    // Open the popup window
+    var popup = window.open("popup.html", "Popup", popupProperties);
+}
