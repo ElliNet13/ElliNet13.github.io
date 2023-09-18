fdbc6c4ad061ed2c1207e5ec3823abc8a55e49c7
ElliNet13 (PC) <elliotmoose4@gmail.com>
2023-09-18 17:47:49 -0400

    No need for https://codemmmmmmmmhehe.github.io

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Service Worker script
+
+const CACHE_NAME = location.origin.replace(/^https?:\/\//, ''); // Remove "https://" or "http://"
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/sw.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
